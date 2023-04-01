0269500e706de6ac8739e9a2ee685a1433e6cc69
Acuña <marcosjuako@hotmail.com>
2023-04-01 00:43:25 -0300

    cors add in product endpoint

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -2,11 +2,12 @@ import methods from 'micro-method-router'
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { middlewareRequest } from 'lib/request';
 import { searchProductsById } from 'controllers/products'
-
+import { handlerCORS } from 'lib/middleware';
 const handle = methods({
   async get(req:NextApiRequest, res:NextApiResponse,id) {
     const results = await searchProductsById(id);
     res.json(results)
   }
 })
-export default middlewareRequest(handle);
+const handleProduct = middlewareRequest(handle);
+export default handlerCORS(handleProduct);
